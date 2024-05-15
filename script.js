50ca66e3287d32b65d24a1f73611a6171fe14918
Md Rony <mdronyrsk01724@gmail.com>
2024-05-15 19:39:15 +0600

    implement date in mov

--- script.js
+++ script.js
@@ -7,9 +7,22 @@
 // Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -17,23 +30,22 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -61,18 +73,27 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = (movements, sort = false) => {
+const displayMovements = (acc, sort = false) => {
   containerMovements.innerHTML = '';
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
+
   movs.forEach((mov, i) => {
+    const now = new Date(acc.movementsDates[i]);
+    const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    const date = `${now.getDate()}`.padStart(2, 0);
+    const year = now.getFullYear();
+
+    const displayDate = `${month} / ${date} / ${year}`;
     // console.log(mov);
     const html = `
     <div class="movements__row">
       <div class="movements__type movements__type--${
         mov > 0 ? 'deposit' : 'withdrawal'
       }">${i + 1} ${mov > 0 ? 'Deposit' : 'Withdrawal'}</div>
-       <div class="movements__date">2 days ago</div>
-         <div class="movements__value">${mov}€</div>
+       <div class="movements__date">${displayDate}</div>
+         <div class="movements__value">${mov.toFixed(2)}€</div>
            </div>
 `;
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -92,7 +113,7 @@ const createUserNames = accs => {
 createUserNames(accounts);
 
 const updateUi = acc => {
-  displayMovements(acc.movements);
+  displayMovements(acc);
   printBlance(acc);
   calcDisplaySummury(acc);
 };
@@ -104,7 +125,7 @@ const printBlance = acc => {
 
   // console.log(blance);
 
-  labelBalance.textContent = `${acc.blance}€`;
+  labelBalance.textContent = `${acc.blance.toFixed(2)}€`;
 };
 
 const calcDisplaySummury = acc => {
@@ -123,7 +144,7 @@ const calcDisplaySummury = acc => {
     .map(mov => (mov * acc.interestRate) / 100)
     .filter(mov => mov >= 1)
     .reduce((acc, cur) => acc + cur, 0);
-  labelSumInterest.textContent = `${interest} €`;
+  labelSumInterest.textContent = `${interest.toFixed(2)} €`;
 };
 
 // console.log(accounts);
@@ -144,6 +165,16 @@ btnLogin.addEventListener('click', e => {
     labelWelcome.textContent = `welcome to ${
       currentAccount.owner.split(' ')[0]
     }`;
+    // Implement Date
+    const now = new Date();
+    const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    const date = `${now.getDate()}`.padStart(2, 0);
+    const year = now.getFullYear();
+    const hours = now.getHours();
+    const min = now.getMinutes();
+
+    labelDate.textContent = `${month} / ${date} / ${year}, ${hours}:${min}`;
+
     updateUi(currentAccount);
   } else {
     containerApp.style.opacity = 0;
@@ -166,6 +197,8 @@ btnTransfer.addEventListener('click', e => {
   ) {
     currentAccount.movements.push(-amount);
     transfer.movements.push(amount);
+    currentAccount.movementsDates.push(new Date());
+    transfer.movementsDates.push(new Date());
 
     updateUi(currentAccount);
   }
@@ -189,9 +222,10 @@ btnClose.addEventListener('click', e => {
 
 btnLoan.addEventListener('click', e => {
   e.preventDefault();
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
   if (amount > 0 && currentAccount.movements.some(mov => mov > amount * 0.1)) {
     currentAccount.movements.push(amount);
+    currentAccount.movementsDates.push(new Date());
     updateUi(currentAccount);
   }
 
@@ -425,35 +459,40 @@ const currencies = new Map([
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
-const dogFood = dogs => {
-  dogs.forEach(dog => (dog.recommendedFood = dog.weight ** 0.75 * 28));
+// const dogFood = dogs => {
+//   dogs.forEach(dog => (dog.recommendedFood = dog.weight ** 0.75 * 28));
 
-  const sarah = dogs.find(dog => dog.owners.some(value => value === 'Sarah'));
+//   const sarah = dogs.find(dog => dog.owners.some(value => value === 'Sarah'));
 
-  sarah.curFood > sarah.recommendedFood
-    ? console.log('its eating too much')
-    : console.log('its eating too little');
+//   sarah.curFood > sarah.recommendedFood
+//     ? console.log('its eating too much')
+//     : console.log('its eating too little');
 
-  const ownersEatTooMuch = [];
-  const ownersEatTooLittle = [];
+//   const ownersEatTooMuch = [];
+//   const ownersEatTooLittle = [];
 
-  dogs.map(dog =>
-    dog.curFood > dog.recommendedFood
-      ? ownersEatTooMuch.push(dog.owners)
-      : ownersEatTooLittle.push(dog.owners)
-  );
+//   dogs.map(dog =>
+//     dog.curFood > dog.recommendedFood
+//       ? ownersEatTooMuch.push(dog.owners)
+//       : ownersEatTooLittle.push(dog.owners)
+//   );
 
-  // ownersEatTooMuch.flat();
+//   // ownersEatTooMuch.flat();
 
-  return dogs;
-};
+//   return dogs;
+// };
 
-const dog = [
-  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
-  { weight: 8, curFood: 200, owners: ['Matilda'] },
-  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
-  { weight: 32, curFood: 340, owners: ['Michael'] },
-];
+// const dog = [
+//   { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+//   { weight: 8, curFood: 200, owners: ['Matilda'] },
+//   { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+//   { weight: 32, curFood: 340, owners: ['Michael'] },
+// ];
 
-console.log(dogFood(dog));
-// console.log(dog);
+// console.log(dogFood(dog));
+// // console.log(dog);
+
+// create a date
+
+// const now = new Date();
+// console.log(now);
